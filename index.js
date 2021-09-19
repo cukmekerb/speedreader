b76683964b792b41ef8f14e732466b8143274c38
cukmekerb <cukmekerb@gmail.com>
2021-09-18 17:13:28 -0700

    add license, and fix issues and stuff. closes #1 closes #2 closes #3

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ var currentinterval;
 var pauselengths;
 var pauseatcomma;
 var pauseatdot;
+
 // keymaster custom filter:
 key.filter = (event) => {
     var tagName = (event.target || event.srcElement).tagName;
     return (!(tagName == 'INPUT' || tagName == 'SELECT' || tagName == 'TEXTAREA')) || (key.isPressed("command") && key.isPressed("enter")) || (key.isPressed("ctrl") && key.isPressed("enter"));
-}
+};
 
 key("ctrl+enter, command+enter", () => {
     if (!paused && currentw == 0) {
         vroom();
     }
-})
+});
 
 key("space", () => {
     paused = !paused;
@@ -25,22 +26,24 @@ key("space", () => {
 });
 
 key("right", () => {
-    if(currentw + 10 >= words.length){
+    if (currentw + 10 >= words.length) {
         currentw = words.length-1;
     }
     else {
-        currentw += 10
+        currentw += 10;
     }
+    document.getElementById("oph2").innerText = words[currentw];
 });
 
 key("left", () => {
-    if (currentw - 10 < 0){
+    if (currentw - 10 < 0) {
         currentw = 0;
     }
     else {
         currentw -= 10;
     }
-})
+    document.getElementById("oph2").innerText = words[currentw];
+});
 
 // prevent scrolling with spacebar
 window.addEventListener('keydown', function (e) {
@@ -48,6 +51,7 @@ window.addEventListener('keydown', function (e) {
         e.preventDefault();
     }
 });
+
 function vroom() {
     if (document.getElementById("area").value && document.getElementById("area").value != "") {
         document.getElementById("oph2").scrollIntoView();
@@ -59,12 +63,13 @@ function vroom() {
         if (currentinterval) {
             clearTimeout(currentinterval);
         }
-        words = words.filter(function (val) { return val !== null && val != "" && val != "\n"; })
+        words = words.filter(function (val) { return val !== null && val != "" && val != "\n"; });
         document.getElementById("oph2").innerText = words[0];
         paused = false;
-        setTimeout(() => { beginflashes(speed) }, 350);
+        setTimeout(() => { beginflashes(speed); }, 350);
     }
 }
+
 function beginflashes(speed) {
     var nspeed = speed;
     if (!paused) {
@@ -73,13 +78,13 @@ function beginflashes(speed) {
             currentinterval = null;
             currentw = 0;
             document.getElementById('signup').scrollIntoView();
-            return
+            return;
         }
         document.getElementById("oph2").innerText = words[currentw];
         if (pauseatdot && words[currentw].match(/\.$|\。$/)) {
             nspeed += pauselengths.dot * 1000;
         }
-        if (pauseatcomma && words[currentw].match(/\,$|\;$|\:$/)) {
+        if (pauseatcomma && words[currentw].match(/\,$|\;$|\:$|\"$|\”$|\“$|\]$|\}$|\)$/)) {
             nspeed += pauselengths.comma * 1000;
         }
         currentw++;
@@ -88,21 +93,24 @@ function beginflashes(speed) {
         beginflashes(speed, pauseatdot, pauseatcomma);
     }, nspeed);
 }
+
 function showadv() {
     document.getElementById("advanceds").classList.remove("hidden");
     document.getElementById("advanced").innerHTML = "less";
     document.getElementById("advanced").setAttribute("onclick", "hidedadv()");
 }
+
 function hidedadv() {
     document.getElementById("advanceds").classList.add("hidden");
     document.getElementById("advanced").innerHTML = "more";
     document.getElementById("advanced").setAttribute("onclick", "showadv()");
 }
+
 function updatepauselen() {
     pauselengths = {
         comma: Number(document.getElementById("comma_pause_len").value),
         dot: Number(document.getElementById("dot_pause_len").value)
-    }
+    };
     pauseatcomma = document.getElementById("pauseatcomma").checked;
     pauseatdot = document.getElementById("pauseatdot").checked;
     if (pauseatcomma) {
@@ -123,9 +131,10 @@ function updatepauselen() {
         wpm: Number(document.getElementById("wpm").value),
         dpl: pauselengths.dot,
         cpl: pauselengths.comma
-    }
+    };
     Cookies.set("settings", JSON.stringify(settings), { expires: 100000 });
 }
+
 function init() {
     document.getElementById('signup').scrollIntoView();
     if (Cookies.get("settings")) {
